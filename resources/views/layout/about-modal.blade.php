db376135177d3e916811dc7c9639682e96d8a4d8
bgsw0709 <agungwiryadika45@gmail.com>
2021-06-04 09:40:14 +0800

    add about modal

---
+++ resources/views/layout/about-modal.blade.php
@@ -0,0 +1,21 @@
+<div class="modal fade" id="aboutApp" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Tentang aplikasi</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Developed by</div>
+            <h4 class="h5 mb-4 font-weight-bold text-gray-800" id="namaDesa">Bagus Wiryadika - 1805551059</h1>
+            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">GitHub</div>
+            <h4 class="h5 mb-4 font-weight-bold text-gray-800" id="namaDesa" href="https://github.com/bagushikano">Here</h1>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+        </div>
+      </div>
+    </div>
+  </div>
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -67,6 +67,12 @@
                 </form>
             </li>
 
+            <li class="nav-item" id="about">
+                <a class="nav-link" onclick='$("#aboutApp").modal("show");' return false;'>
+                    <i class="fas fa-fw fa-info"></i>
+                    <span>Tentang aplikasi</span></a>
+            </li>
+
             <!-- Divider -->
             <hr class="sidebar-divider">
 
@@ -115,25 +121,7 @@
         <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('/layout/about-modal')
 
     <!-- Bootstrap core JavaScript-->
     <script src="{{asset('template/sbadmin/vendor/jquery/jquery.min.js')}}"></script>
